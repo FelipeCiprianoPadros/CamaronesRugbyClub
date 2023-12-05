95069fbade55e7b64c3ab35258f0fc11e6a23d81
FelipeCiprianoPadros <135554676+FelipeCiprianoPadros@users.noreply.github.com>
2023-12-05 13:20:15 -0300

    Version 1.0
    Ciro te dejo el front maquetado, lo unico que hice fue crear el proyecto angular y los directorios, luego no toque nada mas.
    
    Co-Authored-By: Ciro Bernal <107817467+CiroBP@users.noreply.github.com>

--- clubManagment/src/main/java/com/camarones/clubManagment/repository/OperadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.camarones.clubManagment.repository;
-
-import com.camarones.clubManagment.model.Operador;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OperadorRepository extends CrudRepository<Operador, Integer> {
-}
