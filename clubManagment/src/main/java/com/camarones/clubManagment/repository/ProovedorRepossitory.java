8d9a2dc27c8af0bfc07f2598f69175ef537e0a30
FelipeCiprianoPadros <135554676+FelipeCiprianoPadros@users.noreply.github.com>
2023-12-05 21:00:25 -0300

    1.4
    Ultima version del diagrama de clases
    
    Co-Authored-By: Ciro Bernal <107817467+CiroBP@users.noreply.github.com>

--- clubManagment/src/main/java/com/camarones/clubManagment/repository/ProovedorRepossitory.java
+++
@@ -1,7 +0,0 @@
-package com.camarones.clubManagment.repository;
-
-import com.camarones.clubManagment.model.Proovedor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProovedorRepossitory extends CrudRepository<Proovedor,Integer> {
-}
