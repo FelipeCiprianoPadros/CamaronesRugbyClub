ed287e8fd4c35116a9fc071e2259bb1ec474ede4
CiroBP <tpa.11.pina@gmail.com>
2023-12-09 21:15:56 -0300

    incio de front de home

--- codigo/clubFrontEnd/FrontCamarones/Administracion/src/app/app-routing.module.ts
+++ codigo/clubFrontEnd/FrontCamarones/Administracion/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { ProveedorComponent } from "./component/proveedor/proveedor.component";
 import { SocioComponent } from "./component/socio/socio.component";
 import { AppComponent } from "./app.component";
 import { NgModule } from "@angular/core";
+import { LoginComponent } from "./component/login/login.component";
+import { HomeComponent } from "./component/home/home.component";
 
 
 const routes: Routes= [
@@ -14,7 +16,9 @@ const routes: Routes= [
     {path: 'operador', component:OperadorComponent },
     {path: 'proveedor', component:ProveedorComponent },
     {path: 'socio', component:SocioComponent },
-    {path: 'inicio', component:AppComponent}
+    {path: 'inicio', component:AppComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'home', component:HomeComponent}
 ];
 
 @NgModule({
